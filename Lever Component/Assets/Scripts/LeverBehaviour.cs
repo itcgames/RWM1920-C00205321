ce929d02c94f54cc9ebcea514254f202bf59703e
keithw96 <c00205321@itcarlow.ie>
2019-11-24 16:33:54 +0000

    Click Detection Implemented

--- Lever Component/Assets/Scripts/LeverBehaviour.cs
+++ Lever Component/Assets/Scripts/LeverBehaviour.cs
@@ -4,15 +4,25 @@ using UnityEngine;
 
 public class LeverBehaviour : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetMouseButtonDown(0))
+        {
+
+            RaycastHit2D hit = Physics2D.Raycast(Camera.main.ScreenToWorldPoint(Input.mousePosition), Vector2.zero);
+
+            if (hit.transform != null)
+            {
+                printName(hit.transform.gameObject);
+            }
+
+        }
+    }
+
+    private void printName(GameObject go)
+    {
+        print(go.name);
     }
 }
