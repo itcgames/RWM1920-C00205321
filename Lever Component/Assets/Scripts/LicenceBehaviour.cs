52e0d5f86850ef6071d3174e7e99c2c261b6d6fc
keithw96 <c00205321@itcarlow.ie>
2019-12-27 16:13:59 +0000

    Licence Screen Added

---
+++ Lever Component/Assets/Scripts/LicenceBehaviour.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LicenceBehaviour : MonoBehaviour
+{
+
+    
+    void Update()
+    {
+        if (Input.anyKey)
+        {
+            SceneManager.LoadScene(2);
+        }
+    }
+}
