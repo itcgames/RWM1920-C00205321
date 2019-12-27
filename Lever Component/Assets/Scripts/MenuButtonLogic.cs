86071d05b2ed42274192329ffbf69bd27a45ddba
keithw96 <c00205321@itcarlow.ie>
2019-12-27 17:12:59 +0000

    Main Menu, Options and Help added

---
+++ Lever Component/Assets/Scripts/MenuButtonLogic.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuButtonLogic: MonoBehaviour
+{
+    public void GoToMainMenu()
+    {
+        SceneManager.LoadScene(2);
+    }
+
+    public void GoToOptions()
+    {
+        SceneManager.LoadScene(3); //Options
+    }
+
+    public void GoToHelp()
+    {
+        SceneManager.LoadScene(4); //Help 
+    }
+
+    public void GoToPlay()
+    {
+        SceneManager.LoadScene(5); //Game scene
+    }
+}
